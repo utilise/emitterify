0f30afebc4da04395713c4f4031516f3e0202c1a
pemrouz <p3pedram@hotmail.com>
2017-05-14 17:18:00 +0100

    fix: production fixes

--- index.js
+++ index.js
@@ -20,20 +20,24 @@ module.exports = function emitterify(body) {
     for (var i = 0; i < body.on['*'].length; i++)
       call(body.on['*'][i], [type, pm])
 
+    if (li.source) call(li.source, pm)
+
     return body
   }
 
+
   function call(cb, pm){
-      cb.next                                   ? cb.next(pm) 
-    : pm && pm.length && typeof pm !== 'string' ? cb.apply(body, pm) 
-                                                : cb.call(body, pm) 
+      cb.next             ? cb.next(pm) 
+    : pm instanceof Array ? cb.apply(body, pm) 
+                          : cb.call(body, pm) 
   }
 
-  function on(type, callback) {
-    var cb = callback ? callback : observable()
-      , id = type.split('.')[0]
+  function on(type, cb) {
+    var id = type.split('.')[0]
       , li = body.on[id] = body.on[id] || []
       , i = li.length
+
+    if (!cb) return body.on[type].source || (body.on[type].source = observable())
 
     if ((cb.ns = type.split('.')[1]))
       while (~--i && li[i].ns === cb.ns)
@@ -43,17 +47,20 @@ module.exports = function emitterify(body) {
     return cb.next ? cb : body
   }
 
-  function off(type, callback) {
-    var li = (body.on[type] || [])
-      , i  = li.length
-
-    while (~--i && (callback == li[i] || !callback))
-      li.splice(i, 1)
-  }
-
   function once(type, callback){
     (callback = callback || observable()).once = true
     return body.on(type, callback)
+  }
+
+  function off(type, cb) {
+    var li = (body.on[type] || [])
+      , i  = li.length
+
+    if (cb && cb == li.source) 
+      return delete li.source
+
+    while (~--i && (cb == li[i] || !cb))
+      li.splice(i, 1)
   }
 
   function observable() {
@@ -63,29 +70,41 @@ module.exports = function emitterify(body) {
 
     o.map = function(fn) {
       var n = observable()
-      o.listeners.push(function(d, i){ n.next(fn(d, i)) })
+      o.listeners.push(function(d, i){ n.next(fn(d, i, n)) })
+      o.listeners[o.listeners.length - 1].fn = fn
       return n
     }
 
     o.filter = function(fn) {
       var n = observable()
-      o.listeners.push(function(d, i){ fn(d, i) && n.next(d) })
+      o.listeners.push(function(d, i){ fn(d, i, n) && n.next(d) })
+      o.listeners[o.listeners.length - 1].fn = fn
       return n
     }
 
     o.reduce = function(fn, seed) {
       var n = observable()
-      o.listeners.push(function(d, i){ n.next(seed = fn(seed, d, i)) })
+      o.listeners.push(function(d, i){ n.next(seed = fn(seed, d, i, n)) })
+      o.listeners[o.listeners.length - 1].fn = fn
       return n
     }
 
     o.next = function(d) {
-      if (d instanceof Promise) return d.then(o.next), o
       o.resolve(d)
-      o.listeners.map(function(fn){ fn(d, o.i++) })
+      o.listeners.map(function(fn){ fn(d, o.i) })
+      o.i++
+      return o
+    }
+
+    o.off = function(fn){
+      var i = o.listeners.length
+
+      while (~--i && (fn == o.listeners[i].fn || !fn))
+        o.listeners.splice(i, 1)
       return o
     }
 
     return o
+
   }
 }
